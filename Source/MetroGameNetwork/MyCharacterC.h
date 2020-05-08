838e537aa0cfc88ccf335737345d815c8b674301
m18n <37105504+m18n@users.noreply.github.com>
2020-05-08 12:57:43 +0300

    script

--- Source/MetroGameNetwork/MyCharacterC.h
+++ Source/MetroGameNetwork/MyCharacterC.h
@@ -1,9 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
-#define WIN32_LEAN_AND_MEAN
-#pragma comment(lib,"ws2_32.lib")
-#include<WinSock2.h>
+
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
 #include "MyCharacterC.generated.h"
@@ -16,18 +14,16 @@ class METROGAMENETWORK_API AMyCharacterC : public ACharacter
 public:
 	// Sets default values for this character's properties
 	AMyCharacterC();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
-protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
-	UFUNCTION(BlueprintCallable, Category = "Connection")
-	int Connections();
-private:
-	SOCKET Connection;
-	
 
 };
